01fd3ff5106a0fada69726710db27bb5c77bb5b7
Nuelwhiz <emmanuelifeanyi668@gmail.com>
2026-06-17 23:25:28 +0100

    deleted un-usefull button in landin page

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,10 +1,8 @@
 import { Link } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
 
 function Landing() {
   return (
     <>
-      <ToastContainer />
       <main className="h-screen w-full bg-cyan-50 relative ">
         <header className="w-full p-4 flex justify-end">
           <Link
@@ -13,24 +11,9 @@ function Landing() {
           >
             login/signup
           </Link>
-
-          <Link
-            to="/User"
-            className="bg-green-900 px-4 py-2 rounded-2xl text-white"
-          >
-            home
-          </Link>
-
-          <button
-            onClick={() =>
-              toast.success("clicked successfully", {
-                position: "top-center",
-              })
-            }
-          >
-            handle
-          </button>
         </header>
+
+        <h1> welcome to my page</h1>
       </main>
     </>
   );
